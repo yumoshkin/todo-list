e0e55b6eb89737c356a8c42bf0a171c56d168438
Yury Moshkin <yumoshkin@gmail.com>
2021-10-27 22:55:08 +0300

    update files

--- src/components/ItemStatusFilter/ItemStatusFilter.jsx
+++ src/components/ItemStatusFilter/ItemStatusFilter.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 
-import './ItemStatusFilter.css';
-
 const ItemStatusFilter = (props) => {
   const { filter, onChange } = props;
 
